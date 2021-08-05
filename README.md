dd5de1c8cc721fd8a7a7a9aab7a1799e8018c9e3
Abdelali12-codes <jadelmoulaa3@gmail.com>
2021-08-05 17:13:19 +0100

    new changes

--- README.md
+++ README.md
@@ -11,3 +11,9 @@
 - sudo service jenkins start
 
 - sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
+
+
+sudo apt-get install curl
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install -y nodejs
--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h1> Helllo World My name is abdelali</h1>
         </p>
         <a
           className="App-link"
