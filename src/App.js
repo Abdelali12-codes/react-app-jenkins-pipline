da65c83c8503685f2d75ad822879700886f783f3
Abdelali Jadelmoula <67081878+Abdelali12-codes@users.noreply.github.com>
2021-08-09 12:57:09 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
 <h3> This is a reactapp pipline using docker ansible and jenkins </h3>
 <h4> Yeeah That is coooool Nice </h4>
 <h5> It is did not work </h5>
+<h6> The problem is solved </h6>
         </p>
         <a
           className="App-link"
