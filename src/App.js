87b03c70e5ebf2c1cbfba393c8841228c934764d
Abdelali Jadelmoula <67081878+Abdelali12-codes@users.noreply.github.com>
2021-08-09 15:23:50 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,12 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <h1> Helllo World My name is abdelali</h1>
-          <h2> This is a demo of CI/CD</h2>
-<h3> This is a reactapp pipline using docker ansible and jenkins </h3>
-<h4> Yeeah That is coooool Nice </h4>
-<h5> It is did not work </h5>
-<h6> The problem is solved coooooooooooooooool </h6>
+       Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
